03d927c5700efddbf3b780e7f94e7ca7dc3b20a5
higorcarvalho <felype.cet.15@gmail.com>
2019-05-17 17:01:10 -0300

    adding a restaurants feature to the project

---
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,14 @@
+export interface Restaurant {
+    id: string, 
+    name: string,
+    category: string,
+    deliveryEstimate: string,
+    rating: number,
+    imagePath: string
+    /*"id": "bread-bakery",
+    "name": "Bread & Bakery",
+    "category": "Bakery",
+    "deliveryEstimate": "25m",
+    "rating": 4.9,
+    "imagePath": "assets/img/restaurants/breadbakery.png"*/
+}
---
+++ src/app/restaurants/restaurants.service.ts
@@ -0,0 +1,16 @@
+import { Restaurant } from "./restaurant/restaurant.model";
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { MEAT_API } from "../app.api"
+import { Observable } from "rxjs/Observable"
+import 'rxjs/add/operator/map'
+
+@Injectable()
+export class RestaurantsService {
+
+    constructor(private http: Http){}
+
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+    }
+}
