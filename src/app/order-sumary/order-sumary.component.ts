daf9dc059351f22bbf1306e89ea13fc759445eb8
higor21 <felype.cet.15@gmail.com>
2019-07-10 15:36:38 -0300

    Meat App finished

--- src/app/order-sumary/order-sumary.component.ts
+++ src/app/order-sumary/order-sumary.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderSumaryComponent implements OnInit {
 
+  rated: boolean
+
   constructor() { }
 
   ngOnInit() {}
+
+  rate(){
+    this.rated = true
+  }
 }
